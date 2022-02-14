d08e9dd60c8c6b7c4cda79e6ed0f0ea33e775d4d
Ryan Esteves <ryan.esteves@ct.gov>
2022-02-14 13:56:43 -0500

    Added benchmark for integer based state machine

---
+++ FastState.Benchmarks/IntegerStateMachine.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+
+using FastState;
+
+public class IntegerStateMachine
+{
+    readonly StateMachine<int, int> stringStateMachine = new(builder =>
+    {
+        builder.From(1)
+
+            .When(input => input == 1, 2)
+            .When(input => input<1, 3)
+
+            .When(input => input<2, 2)
+            .When(input => input == 2, 3)
+
+            .When(input => input == 3, 2)
+            .When(3, 3)
+
+            .Default(4);
+
+        builder.From(2);
+    });
+
+    [Benchmark]
+    public int TryTransitionStringStateHitConstant()
+    {
+        stringStateMachine.TryTransition(1, 3, out int newState);
+        return newState;
+    }
+
+    [Benchmark]
+    public int TryTransitionStringStateHitExpression()
+    {
+        stringStateMachine.TryTransition(1, 1, out int newState);
+        return newState;
+    }
+
+    [Benchmark]
+    public int TryTransitionStringStateDefault()
+    {
+        stringStateMachine.TryTransition(1, 4, out int newState);
+        return newState;
+    }
+
+    [Benchmark]
+    public int TryGetDefaultForStateStringStateHit()
+    {
+        stringStateMachine.TryGetDefaultForState(1, out int newState);
+        return newState;
+    }
+
+    [Benchmark]
+    public int TryGetDefaultForStateStringStateMiss()
+    {
+        stringStateMachine.TryGetDefaultForState(2, out int newState);
+        return newState;
+    }
+}
