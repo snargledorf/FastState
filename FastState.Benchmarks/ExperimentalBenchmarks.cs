6be29c265b978d29bf41789f676fa205d95ba2c0
Ryan Esteves <ryan.esteves@ct.gov>
2022-02-25 08:49:07 -0500

    Test delegate against direct call

--- FastState.Benchmarks/ExperimentalBenchmarks.cs
+++ FastState.Benchmarks/ExperimentalBenchmarks.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Linq.Expressions;
+
 using BenchmarkDotNet.Attributes;
 
 namespace FastState.Benchmarks
@@ -8,90 +9,81 @@ namespace FastState.Benchmarks
         const int numberOfStatesAndInputs = 5;
         const int delStateOffset = 50;
 
-        protected static readonly IEnumerable<int> stateInts = Enumerable.Range(1, numberOfStatesAndInputs);
+        protected static readonly IEnumerable<int> states = Enumerable.Range(1, numberOfStatesAndInputs);
+
+        private delegate int GetNewStateDel(int state, int input);
+
+        private GetNewStateDel? getNewState;
 
         [GlobalSetup]
         public void Setup()
         {
+            CompileGetNewState();
+        }
+
+        private void CompileGetNewState()
+        {
+            ParameterExpression stateParam = Expression.Parameter(typeof(int));
+            ParameterExpression inputParam = Expression.Parameter(typeof(int));
+            LabelTarget returnTarget = Expression.Label(typeof(int));
+
+            var currentStateFlowControlExpressions = new List<Expression>();
+
+            SwitchCase[] inputSwitchCases = states.Select(state1 => Expression.SwitchCase(Expression.Return(returnTarget, Expression.Constant(state1)), Expression.Constant(state1))).ToArray();
+
+            var stateSwitchCase = Expression.SwitchCase(
+                Expression.Switch(inputParam, inputSwitchCases),
+                states.Select(state => Expression.Constant(state)).ToArray()
+            );
+
+            currentStateFlowControlExpressions.Add(Expression.Switch(stateParam, stateSwitchCase));
+
+            currentStateFlowControlExpressions.Add(Expression.Throw(Expression.Constant(new ArgumentException("Invalid state"))));
+            currentStateFlowControlExpressions.Add(Expression.Label(returnTarget, Expression.Constant(0, typeof(int))));
+
+            BlockExpression body = Expression.Block(currentStateFlowControlExpressions);
+
+            getNewState = Expression.Lambda<GetNewStateDel>(body, stateParam, inputParam).Compile();
         }
 
         [Benchmark]
-        public int TryTransitionHitConstant()
+        public int TryTransitionHitConstantDelegate()
         {
             int result = default;
 
-            foreach (var state in stateInts)
+            foreach (var state in states)
             {
-                result = GetNewState(state, state);
+                result = getNewState!(state, state);
+                if (result != state)
+                    throw new Exception();
             }
 
             return result;
         }
 
-        private int GetNewState(int state, int input)
+        [Benchmark]
+        public int TryTransitionHitConstantDirectCall()
+        {
+            int result = default;
+
+            foreach (var state in states)
+            {
+                result = GetNewState(state, state);
+                if (result != state)
+                    throw new Exception();
+            }
+
+            return result;
+        }
+
+        private static int GetNewState(int state, int input)
         {
             switch (state)
             {
                 case 1:
-                    switch (input)
-                    {
-                        case 1:
-                            return 1;
-                        case 2:
-                            return 2;
-                        case 3:
-                            return 3;
-                        case 4:
-                            return 4;
-                        case 5:
-                            return 5;
-                    }
-                    break;
                 case 2:
-                    switch (input)
-                    {
-                        case 1:
-                            return 1;
-                        case 2:
-                            return 2;
-                        case 3:
-                            return 3;
-                        case 4:
-                            return 4;
-                        case 5:
-                            return 5;
-                    }
-                    break;
                 case 3:
-                    switch (input)
-                    {
-                        case 1:
-                            return 1;
-                        case 2:
-                            return 2;
-                        case 3:
-                            return 3;
-                        case 4:
-                            return 4;
-                        case 5:
-                            return 5;
-                    }
-                    break;
                 case 4:
-                    switch (input)
-                    {
-                        case 1:
-                            return 1;
-                        case 2:
-                            return 2;
-                        case 3:
-                            return 3;
-                        case 4:
-                            return 4;
-                        case 5:
-                            return 5;
-                    }
-                    break;
                 case 5:
                     switch (input)
                     {
@@ -109,7 +101,26 @@ namespace FastState.Benchmarks
                     break;
             }
 
-            throw new Exception();
+            throw new Exception($"Unexpected state {state}");
+        }
+
+        private int Func_1(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                case 3:
+                    return 3;
+                case 4:
+                    return 4;
+                case 5:
+                    return 5;
+            }
+
+            throw new Exception($"Unexpected input {input}");
         }
     }
 }
